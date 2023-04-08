b8d87928816ac790af2fcf0c49a2d28ef400efde
Kamil <67270768+dhrkaydin@users.noreply.github.com>
2023-04-08 11:33:48 +0200

    init
    initial template for the spring portion of the application which i generated using spring initializr

---
+++ src/main/java/com/assessment/recipetool/RecipetoolApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.recipetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipetoolApplication.class, args);
+	}
+
+}
